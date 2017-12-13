6444648c8fc336c22d92be9042cff4dd904a3140
Nathan Metzger <linuxfan724@gmail.com>
2017-12-12 21:23:34 -0500

    Update script.js

--- TBC/script.js
+++ TBC/script.js
@@ -339,7 +339,7 @@ function period10() {
 }
 function visibility() {
     document.getElementById('confirmation').style.visibility = "visible";
-    if (windowlocation == "") {
+    if (redirectlocation == "") {
         document.getElementById("confirmationbutton").style.visibility = "hidden";
         document.getElementById("confirmationtext").innerHTML = "No code was specified for this period. If this is incorrect, rewrite it at cookiewriter";
     }
